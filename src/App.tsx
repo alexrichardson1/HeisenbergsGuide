e4cfd10655b8a037365dd6a5582f5ed88e9ecb44
R, Alex <alex.richardson19@imperial.ac.uk>
2021-07-26 19:30:07 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+// import * as React from "react";
+// import * as ReactDOM from "react-dom";
+// ReactDOM.render(
+//   <div>
+//     <h1>Hello, Welcome to the first page</h1>
+//   </div>,
+//   document.getElementById("root")
+// );
+
+export default App;
