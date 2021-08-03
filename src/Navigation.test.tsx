b18b1591948b2ad0ff0279e7a7b25db5c124418f
R, Alex <alex.richardson19@imperial.ac.uk>
2021-08-03 13:07:34 +0100

    test(src/): Add Navigation.test.tsx

---
+++ src/Navigation.test.tsx
@@ -0,0 +1,7 @@
+import renderer from "react-test-renderer";
+import Navigation from "./components/Navigation";
+
+test("renders correctly", () => {
+  const tree = renderer.create(<Navigation />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
