4bb662b2f286548c7611929fc2861261d3e71348
R, Alex <alex.richardson19@imperial.ac.uk>
2021-08-03 13:06:16 +0100

    fix(NoPageFound.tsx): Use `BrowserRouter`

--- src/components/NoPageFound.tsx
+++ src/components/NoPageFound.tsx
@@ -1,17 +1,20 @@
 import { LinkContainer } from "react-router-bootstrap";
 import "../styles/NoPageFound.css";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function NoPageFound() {
   return (
-    <div>
-      <LinkContainer to="/">
-        <section className="container">
-          <button data-hover="Go to Home">
-            <div>404 Page Not Found</div>
-          </button>
-        </section>
-      </LinkContainer>
-    </div>
+    <Router>
+      <div>
+        <LinkContainer to="/">
+          <section className="container">
+            <button data-hover="Go to Home">
+              <div>404 Page Not Found</div>
+            </button>
+          </section>
+        </LinkContainer>
+      </div>
+    </Router>
   );
 }
 
