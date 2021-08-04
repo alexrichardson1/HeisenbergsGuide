1beb9d97ed09e6c7b484d5517bc12627c0415ae2
R, Alex <alex.richardson19@imperial.ac.uk>
2021-08-04 10:46:00 +0100

    fix(components/): Remove `BrowserRouter`
    `BrowserRouter` prevented page rendering without refreshing the page

--- src/components/NoPageFound.tsx
+++ src/components/NoPageFound.tsx
@@ -1,20 +1,17 @@
 import { LinkContainer } from "react-router-bootstrap";
 import "../styles/NoPageFound.css";
-import { BrowserRouter as Router } from "react-router-dom";
 
 function NoPageFound() {
   return (
-    <Router>
-      <div>
-        <LinkContainer to="/">
-          <section className="container">
-            <button data-hover="Go to Home">
-              <div>404 Page Not Found</div>
-            </button>
-          </section>
-        </LinkContainer>
-      </div>
-    </Router>
+    <div>
+      <LinkContainer to="/">
+        <section className="container">
+          <button data-hover="Go to Home">
+            <div>404 Page Not Found</div>
+          </button>
+        </section>
+      </LinkContainer>
+    </div>
   );
 }
 
